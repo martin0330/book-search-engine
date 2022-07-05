679a350035a133919df2ca0fc104193ce12c7d6d
martin0330 <martin.cruz0330@yahoo.com>
2022-07-05 15:21:33 -0500

    updated query and fixed app.js

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,28 +1,20 @@
 import { gql } from "@apollo/client";
 
-export const QUERY_ME = gql`
+export const GET_ME = gql`
   {
     me {
       _id
       username
       email
-      friendCount
-      thoughts {
-        _id
-        thoughtText
-        createdAt
-        reactionCount
-        reactions {
-          _id
-          createdAt
-          reactionBody
-          username
-        }
-      }
-      friends {
-        _id
-        username
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
       }
     }
   }
-`; 
+`;
